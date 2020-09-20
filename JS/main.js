7dbb81f29c7184bbd760185ca87f78982efaa0f3
kousuke-bass <70252775+kousuke-bass@users.noreply.github.com>
2020-09-20 23:08:30 +0900

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,97 +1,54 @@
 $(function(){
   //特定の要素に飛ばす
   const profill =$('.profill').offset().top;
-  const skills =$('.webskill').offset().top;
-  const service =$('.service').offset().top;
+  const skill=$('.webskill').offset().top;
+  const services =$('.service').offset().top;
   const works =$('.work').offset().top;
-  const contact =$('.contact').offset().top;
-  $('#about').on('click',function(){
-    $(window).scrollTop(profill);
-    $('.header-list,.menu-trigger').removeClass('is-active');
+  const contacts =$('.contact').offset().top;
 
-  });
-  $('#skills').on('click',function(){
-    $(window).scrollTop(skills);
-    $('.header-list,.menu-trigger').removeClass('is-active');
+//格メニュー項目押した時の、処理
+$('.header-list li').on('click',function(){
+let click=$(this).attr('id');
+let remove = $('.header-list,.menu-trigger');
 
-  });
-  $('#service').on('click',function(){
-    $(window).scrollTop(service);
-    $('.header-list,.menu-trigger').removeClass('is-active');
+if(click=="about"){
+  $(window).scrollTop(profill);
+  $(remove).removeClass('is-active');
+}
+else if (click=="skills") {
+  $(window).scrollTop(skill);
+  $(remove).removeClass('is-active');
 
-  });
-  $('#works').on('click',function(){
-    $(window).scrollTop(works);
-    $('.header-list,.menu-trigger').removeClass('is-active');
+}
+else if (click=="service") {
+  $(window).scrollTop(services);
+  $(remove).removeClass('is-active');
 
-  });
-  $('#contact').on('click',function(){
-    $(window).scrollTop(contact);
-    $('.header-list,.menu-trigger').removeClass('is-active');
+}
+else if (click=="works") {
+  $(window).scrollTop(works);
+  $(remove).removeClass('is-active');
 
-  });
+}
+else if (click=="contact") {
+  $(window).scrollTop(contacts);
+  $(remove).removeClass('is-active');
+}
+});
 
+//ハンバーガーメニュー
 $('.menu-trigger').on('click',function(){
   $('.menu-trigger').toggleClass('is-active');
   $('.header-list').toggleClass('is-active');
-
 });
 
-//コンテンツを表示
-$(window).scroll(function (){
-  console.log($(this).scrollTop());//スクロールの高さを表示
-    $('.fadein').each(function(){
-        var position = $(this).offset().top;
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        if (scroll > position - windowHeight + 300){
-          $(this).addClass('active');
-        }
-    });
+$('.work-show').on('click',function(){
+  $('.work-show').toggleClass('active');
+  $('.modal,.close-modal').toggleClass('active');
 });
 
-//ページトップ
- var topBtn = $('#page-top');   
-    topBtn.hide();
-    //スクロールが100に達したらボタン表示
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            topBtn.fadeIn();
-        } else {
-            topBtn.fadeOut();
-        }
-    });
-    //スクロールしてトップ
-    topBtn.click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 500);
-        return false;
-    });
-
-//WorkSLIDER
-  $('.work-image').slick({
-    arrows:true,
-    autoplay:true,
-    dots:true,
-    speed:1500,
-    easing:'swing',
-    centerMode:true,
-    centerPadding:'10%',
-    infinite:true,
-    slidesToShow:3,
-  slidesToScroll:1,
-   prevArrow:'<div class="slide-btn prev-btn"></div>',
-   nextArrow:'<div class="slide-btn next-btn"></div>',
-
-   responsive:[{
-     breakpoint:798,
-     settings:{
-       centerPadding:'0%',
-       slidesToShow:1,
-       slideToScroll:1,
-     }
-   }
- ]
-  });
+$('.close-modal').on('click',function(){
+  let show_down =$('.work-show,.modal,.close-modal');
+  show_down.removeClass('active');
 });
+
